4d97e61ca118f9879eea66f0e38ed15e7245a116
loxt <emannuel.matos@begrowth.co>
2021-09-21 15:17:17 -0400

    feat(setup): add custom logger

--- hydra/main.go
+++ hydra/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/loxt/mastering-go-programming/hydra/hlogger"
 )
 
 func main() {
+	logger := hlogger.GetInstance()
+	logger.Println("Starting Hydra web service")
+
 	http.HandleFunc("/", sroot)
-	http.ListenAndServe(":8080", nil)	
+	http.ListenAndServe(":8080", nil)
 }
 
 func sroot(w http.ResponseWriter, r *http.Request) {
+	logger := hlogger.GetInstance()
 	fmt.Fprint(w, "Welcome to the Hydra software system")
+
+	logger.Println("Received an http Get request on root url")
 }
