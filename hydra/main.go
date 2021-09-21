322ad06948cdc9234e1c9eea0884d343ac2d840e
loxt <emannuel.matos@begrowth.co>
2021-09-21 15:29:55 -0400

    feat(setup): add hydra shield builder

--- hydra/main.go
+++ hydra/main.go
@@ -5,11 +5,17 @@ import (
 	"net/http"
 
 	"github.com/loxt/mastering-go-programming/hydra/hlogger"
+	"github.com/loxt/mastering-go-programming/hydra/shieldBuilder"
 )
 
 func main() {
 	logger := hlogger.GetInstance()
 	logger.Println("Starting Hydra web service")
+
+	builder := shieldBuilder.NewShieldBuilder()
+
+	shield := builder.RaiseFront().RaiseBack().Build()
+	logger.Printf("%+v \n", *shield)
 
 	http.HandleFunc("/", sroot)
 	http.ListenAndServe(":8080", nil)
